9579bce24c256e1eba16ff76deeef9018748925a
Matthew Lamm <matthewjlamm@gmail.com>
2020-04-16 15:51:09 -0400

    Sign in on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up!"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end 
